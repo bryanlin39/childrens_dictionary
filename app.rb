938217b2f0e77f691ffad06ce839a6be3e954877
Bryan Lin <bryanlin39@gmail.com>
2017-04-28 13:25:15 -0700

    add HTML to .erb files and use Sinatra with app.rb to link all pages together

---
+++ app.rb
@@ -0,0 +1,36 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+also_reload('lib/**/*.rb')
+require('pry')
+
+get('/') do
+  @words = Word.all()
+  erb(:index)
+end
+
+post('/word_form') do
+  inputted_word = params[:inputted_word].downcase().capitalize()
+  @new_word = Word.new({:word_input=> inputted_word})
+  @new_word.save()
+  @words = Word.all()
+  erb(:index)
+end
+
+get('/word/:id') do
+  word_id = params['id'].to_i()
+  @word = Word.find(word_id)
+  @definitions = @word.definitions()
+  erb(:word)
+end
+
+post('/definition_form/:id') do
+  word_id = params['id'].to_i()
+  @word = Word.find(word_id)
+  inputted_definition = params[:inputted_definition].downcase()
+  @new_definition = Definition.new({:definition_input=> inputted_definition})
+  @word.add_definition(@new_definition)
+  @definitions = @word.definitions()
+  erb(:word)
+end
