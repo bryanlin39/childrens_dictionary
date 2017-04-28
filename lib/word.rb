194fde8eaffe4c69e040e86e38d1b337e4c7892d
Bryan Lin <bryanlin39@gmail.com>
2017-04-28 11:07:18 -0700

    add unit specs and methods for Definition and Word classes

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,39 @@
 class Word
 
-  attr_accessor(:word_input)
+  @@words = []
+
+  attr_accessor(:word_input, :id, :definitions)
 
   def initialize(attributes)
     @word_input = attributes[:word_input]
+    @id = @@words.length() + 1
+    @definitions = []
+  end
+
+  def self.all
+    @@words
+  end
+
+  def save
+    @@words.push(self)
+  end
+
+  def self.clear
+    @@words = []
+  end
+
+  def self.find(id)
+    found_word = nil
+    @@words.each() do |word|
+      if word.id() == id
+        found_word = word
+      end
+    end
+    found_word
+  end
+
+  def add_definition(definition)
+    @definitions.push(definition)
   end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,10 +4,62 @@ require('definition')
 
 describe(Word) do
 
+  before() do
+    Word.clear()
+  end
+
   describe('#word_input') do
     it('returns the inputted word') do
       test_word = Word.new({:word_input=> 'cat'})
       expect(test_word.word_input()).to(eq('cat'))
+    end
+  end
+
+  describe('.all') do
+    it('returns all of the words in the words class variable') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the word into the words class variable') do
+      test_word = Word.new({:word_input=> 'cat'})
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of the word') do
+      test_word1 = Word.new({:word_input=> 'cat'})
+      test_word1.save()
+      test_word2 = Word.new({:word_input=> 'dog'})
+      test_word2.save()
+      expect(test_word2.id()).to(eq(2))
+    end
+  end
+
+  describe('.find') do
+    it('returns the word based on the word id') do
+      test_word = Word.new({:word_input=> 'cat'})
+      test_word.save()
+      expect(Word.find(test_word.id())).to(eq(test_word))
+    end
+  end
+
+  describe('#definitions') do
+    it('returns the definitions associated with the word') do
+      test_word = Word.new({:word_input=> 'cat'})
+      expect(test_word.definitions()).to(eq([]))
+    end
+  end
+
+  describe('#add_definition') do
+    it('saves the definition into the word definitions array') do
+      test_word = Word.new({:word_input=> 'cat'})
+      test_definition = Definition.new({:definition_input=> 'a cute domesticated mammal that is sometimes friendly but more often not'})
+      test_word.add_definition(test_definition)
+      expect(test_word.definitions()).to(eq([test_definition]))
     end
   end
 
